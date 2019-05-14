e1406e2bc03c4ceb838867ce36daeb085d348ffa
Denis Gossa <50202499+DenisGossa@users.noreply.github.com>
2019-05-14 20:50:09 +0300

    Update 4 FriendlyNumbers.java

--- 4 FriendlyNumbers.java
+++ 4 FriendlyNumbers.java
@@ -5,36 +5,36 @@ public class FriendlyNumbers {
 		final int MINBOUNDER=1;
 		final int MAXBOUNDER=20000;
 		
-		/*формируем число в цикле*/
+		/*Formation a first number in cycle*/
 		
 		for(int var=MINBOUNDER;var<MAXBOUNDER;++var) {
 				int firstnum=var;
 				int sumofdeviders1number=0;
 				int sumofdeviders2number=0;
 		
-		/*разложить на делители 1 num*/
+		/*getting devider for 1 num(first number )*/
 				for(int i=1;i<firstnum;i++) {
 									if(firstnum%i==0) {
-											/*посчитать сумму делителей*/
+											/*getting sum of deviders first number*/
 											sumofdeviders1number+=i;
 											continue;
 									} 
 				}
 				if(sumofdeviders1number==firstnum) {
 							continue;
-				}/*пропустить 6;28*/
+				}/*except numbers  6 and 28*/
 		
 		
-		/* получили второе число (сумма делителей 1 num)*/
+		/*  the sum of deviders first number is second number)*/
 				int secondnum=sumofdeviders1number;
 				for(int i=1;i<secondnum;i++) {
 									if(secondnum%i==0) {
-											//посчитать сумму делителей	
+											/*getting sum of deviders second number*/	
 											sumofdeviders2number+=i;
 											continue;
 									} 
 		}
-		/*сравнили значения*/
+		/**/
 		if(sumofdeviders2number==firstnum) {
 			System.out.println(firstnum +" "+sumofdeviders1number+" -Friends numbers");
 		
